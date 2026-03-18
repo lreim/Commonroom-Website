39e136586c2834936dffbe6e8f9fdb6ccd3d38f8
Lissy Reim <lissy@lre.im>
2026-03-18 16:33:58 +0100

    first minimal setup with one homepage

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config
+from flask_socketio import SocketIO
+
+bootstrap = Bootstrap()
+socketio = SocketIO()
+
+#Application factory: erzeugt jedes Mal neu konfiguierte Flask-App 
+#je nachdem, was für (config_name) eingesetzt wird und aufgerufen wird, wier app verschieden konfiguriert 
+def create_app(config_name):
+    app = Flask(__name__)     #erzeugt Flask-Objekt 
+    app.config.from_object(config[config_name])   #lädt alle Variablen des dictioaries abhängig vom fkt argument am Anfang 
+    config[config_name].init_app(app)   #??
+
+    #extensions an app binden, statt nur wie oben zu definieren 
+    bootstrap.init_app(app)
+    socketio.init_app(app)
+    
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)   #hängt alle Routen an die App
+
+    return app
---
+++ config.py
@@ -0,0 +1,29 @@
+import os 
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'  #CSRF Schutz
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+class ProductionConfig(Config):
+    pass
+
+
+config = {
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig,
+    "default": DevelopmentConfig,
+}
+
