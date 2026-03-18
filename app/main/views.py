5c762b994bf22d5c2afb2d2825361a5c17d3a035
Lissy Reim <lissy@lre.im>
2026-03-18 17:03:05 +0100

    add routes and navbar for about and rules

--- app/main/views.py
+++ app/main/views.py
@@ -4,4 +4,14 @@ from flask import render_template
 
 @main.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', active_page='index')
+
+@main.route('/about')
+def about():
+    return render_template('about.html', active_page='about')
+
+
+@main.route('/rules')
+#@login_required
+def rules():
+    return render_template('rules.html', active_page='rules')
