4e48b125fae9dc341f9a50d0cc1faa2c05c80a88
Lissy Reim <lissy@lre.im>
2026-04-15 15:59:34 +0200

    registration basics work

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, Regexp, EqualTo 
+from wtforms import ValidationError 
+#from ..models import User 
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Keep me logged in")
     submit = SubmitField("Let's Lock in ;)")
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(8, 64), Email()])
+    
+    username = StringField('Username', validators=[DataRequired(), Length(max=64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                                                                        'Usernames must have only letters, '
+                                                                                        'numbers, dots or underscores')])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    #def validate_email(self, field):
+    #    if User.query.filter_by(email=field.data).first():
+    #        raise ValidationError('Email already registered.')
+    #def validate_username(self, field):
+    #    if User.query.filter_by(username=field.data).first():
+    #        raise ValidationError('Username already in use.')
+        
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,10 @@
-from flask import flash, redirect, render_template, url_for
-from app.auth import auth
-from app.auth.forms import LoginForm
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user, login_required, logout_user, current_user
+from . import auth     #importiert auth object aus __init__.py
+#from ..models import User 
+from .forms import LoginForm, RegistrationForm #ChangePasswordForm, ChangeEmailForm
+#from .. import db 
+#from ..email import send_email
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
@@ -9,3 +13,16 @@ def login():
         flash(f"{form.email.data} locked in!")
         return redirect(url_for("main.index"))
     return render_template("auth/login.html", form=form)
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        #user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+       # db.session.add(user)
+        #db.session.commit()
+        #token = user.generate_confirmation_token()
+        #send_email(user.email, 'Please Confirm Your Account', 'auth/email/confirm', user=user, token=token)
+        flash('A confirmation email has been sent to you. Pls take a look!')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
