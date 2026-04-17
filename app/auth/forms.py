2a84dbd5e60ff3f06eabd7d2880fc3c58cd22d9a
Lissy Reim <lissy@lre.im>
2026-04-17 17:51:13 +0200

    add registration

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -14,12 +14,12 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(8, 64), Email()])
     
-    username = StringField('Username', validators=[DataRequired(), Length(max=64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+    username = StringField('Username(TODO: given)', validators=[DataRequired(), Length(max=64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                                                                                         'Usernames must have only letters, '
                                                                                         'numbers, dots or underscores')])
     password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', message='Passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
-    submit = SubmitField('Register')
+    submit = SubmitField('Register and join the community! ')
 
     #def validate_email(self, field):
     #    if User.query.filter_by(email=field.data).first():
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,17 +1,21 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, login_required, logout_user, current_user
 from . import auth     #importiert auth object aus __init__.py
-#from ..models import User 
+from ..models import User 
 from .forms import LoginForm, RegistrationForm #ChangePasswordForm, ChangeEmailForm
-#from .. import db 
+from .. import db 
 #from ..email import send_email
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f"{form.email.data} locked in!")
-        return redirect(url_for("main.index"))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            flash(f"{form.email.data} locked in!")
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Welp, invalid username or password, my friend.')
     return render_template("auth/login.html", form=form)
 
 @auth.route('/logout')
@@ -25,11 +29,11 @@ def logout():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        #user = User(email=form.email.data, username=form.username.data, password=form.password.data)
-       # db.session.add(user)
-        #db.session.commit()
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
         #token = user.generate_confirmation_token()
         #send_email(user.email, 'Please Confirm Your Account', 'auth/email/confirm', user=user, token=token)
-        flash('A confirmation email has been sent to you. Pls take a look!')
+        flash('A confirmation email has been sent to you. Please take a look (innert the next hour)!')
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
