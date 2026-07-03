6a80d1a3cd8e16964c7f3427992b9369305f6679
Lissy Reim <lissy@lre.im>
2026-07-03 18:00:02 +0200

    debug off

--- manage.py
+++ manage.py
@@ -1,11 +1,16 @@
 import os
-from app import create_app
+from app import create_app, socketio
 from dotenv import load_dotenv
-from app import create_app
 
 load_dotenv()
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=5001,
+        debug=False,
+        use_reloader=False,
+    )
