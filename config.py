4110f01e30ee5bf6a2abd7c06807b707672ca009
Lissy Reim <lissy@lre.im>
2026-07-05 17:39:29 +0200

    change email domain

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'  #CSRF Schutz
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     TALKTO_MAIL_SUBJECT_PREFIX = '[COMMONROOM]'
-    TALKTO_MAIL_SENDER = 'CommonRoom Developers <lissy.reim@t-online.de>'
+    TALKTO_MAIL_SENDER = os.environ.get('TALKTO_MAIL_SENDER') or 'CommonRoom <noreply@commonroom.ch>'
     TALKTO_ADMIN = os.environ.get('TALKTO_ADMIN')
     TALKTO_POSTS_PER_PAGE = 20
     POSTMARK_API_TOKEN = os.environ.get('POSTMARK_API_TOKEN')
